29fef363db080a14bf223e03ac5629a888c277a7
Lavi Miksa <lmiksa@gorillagroup.com>
2020-04-01 09:03:24 -0400

    adjsuted visibilty

--- src/test/TestStateTableData.java
+++ src/test/TestStateTableData.java
@@ -4,20 +4,19 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.Test;
-
 import model.TableData;
 
-class TestStateTableData {
-	private String path = "/home/lavi/git/covid-19-data/";
+public class TestStateTableData {
+//	private String path = "/home/lavi/git/covid-19-data/";
+	private String path = "/Users/lavi/git/covid-19-data/";
 	private String fileName = "us-states.csv";
-	@Test
-	void testSingleState() throws IOException {
+	@org.junit.Test
+	public void testSingleState() throws IOException {
 		TableData td = new TableData(path+fileName);
 		assertTrue("Washington".equalsIgnoreCase(td.getFiled(1, "state")));
 	}
-	@Test
-	void testDataForSpecificState() throws IOException {
+	@org.junit.Test
+	public void testDataForSpecificState() throws IOException {
 		TableData td = new TableData(path+fileName);
 		assertTrue(td.getDataByColumn("state", "Ohio").size() > 1);		
 	}
